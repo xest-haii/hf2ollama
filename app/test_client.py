fb8fb578ed1b541d02f7a22ee80c655b4d808835
김동한 <xest071@gmail.com>
2024-09-06 16:11:36 +0900

    fix test client errors

--- app/test_client.py
+++ app/test_client.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 
 OPENAI_BASE_URL = os.getenv('OPENAI_BASE_URL', 'http://localhost:8000/v1')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'openai')
-MODEL = os.getenv('MODEL', 'LGAI-EXAONE_EXAONE-3.0-7.8B-Instruct')
+MODEL = os.getenv('MODEL', 'LGAI/EXAONE_EXAONE-3.0-7.8B-Instruct')
 SYSTEM_MESSAGE = os.getenv('SYSTEM_MESSAGE', 'You are a helpful assistant.')
 USER_MESSAGE = os.getenv('USER_MESSAGE', 'Hi')
 STREAM = bool(os.getenv('STREAM', False))
--- app/test_client.sh
+++ app/test_client.sh
@@ -2,24 +2,25 @@
 
 OPENAI_BASE_URL="${OPENAI_BASE_URL:-http://localhost:8000/v1}"
 OPENAI_API_KEY="${OPENAI_API_KEY:-openai}"
-MODEL="${MODEL:-LGAI-EXAONE_EXAONE-3.0-7.8B-Instruct}"
+MODEL="${MODEL:-LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct}"
 SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-You are a helpful assistant.}"
 USER_MESSAGE="${USER_MESSAGE:-Hi}"
 STREAM="${STREAM:-false}"
 
 curl -v ${OPENAI_BASE_URL}/chat/completions \
 	-H "Content-Type: application/json" \
-       	-d "{
-		\"model\": \"${MODEL}\",
-		\"messages\": [
-			{
-				\"role\": \"system\",
-				\"content\": \"${SYSTEM_MESSAGE}\"
-			},
-			{
-				\"role\": \"user\",
-				\"content\": \"${USER_MESSAGE}\"
-			}
-		],
-		\"stream\": ${STREAM}
-	}"
+	-d @- <<EOF
+{
+	"model": "${MODEL}",
+	"messages": [
+		{
+			"role": "system",
+			"content": "${SYSTEM_MESSAGE}"
+		},
+		{
+			"role": "user",
+			"content": "${USER_MESSAGE}"
+		}
+	],
+	"stream": ${STREAM}
+}
